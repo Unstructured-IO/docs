fa97e84494c3f4db4eaac9e167fcc15112aae10c
Maria Khalusova <kafooster@gmail.com>
2024-05-02 12:34:14 -0400

    Script for kapa.ai (#26)

---
+++ script.js
@@ -0,0 +1,7 @@
+<script async
+src="https://widget.kapa.ai/kapa-widget.bundle.js"
+data-website-id="8ae12a97-484a-4704-8127-b6f17ebc6bcf"
+data-project-name="Unstructured"
+data-project-color="#0CDDF8"
+data-project-logo="https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/img/unstructured_logo.png"
+></script>
