97d59d9ba271db3ac11411b0fd51028d58963e6d
Maria Khalusova <kafooster@gmail.com>
2024-05-02 16:21:31 -0400

    Script for kapa.ai update (#27)

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-<script async
-src="https://widget.kapa.ai/kapa-widget.bundle.js"
-data-website-id="8ae12a97-484a-4704-8127-b6f17ebc6bcf"
-data-project-name="Unstructured"
-data-project-color="#0CDDF8"
-data-project-logo="https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/img/unstructured_logo.png"
-></script>
+var script = document.createElement("script");
+script.src = "https://widget.kapa.ai/kapa-widget.bundle.js";
+script.setAttribute("data-website-id", "8ae12a97-484a-4704-8127-b6f17ebc6bcf");
+script.setAttribute("data-project-name", "Unstructured");
+script.setAttribute("data-project-color", "#0CDDF8");
+script.setAttribute(
+  "data-project-logo",
+  "https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/img/unstructured_logo.png"
+);
+document.body.appendChild(script);
